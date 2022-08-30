493184e4f1fddbff32bee05dac449e29a779f28b
Kate Miholenko <katemixa92@gmail.com>
2022-08-31 01:11:20 +0300

    update

--- src/main/java/page/MenuBlock.java
+++ src/main/java/page/MenuBlock.java
@@ -25,7 +25,6 @@ public class MenuBlock extends BasePage {
     private static final By NAME_CATEGORY_LOCATOR = By.className("name");
 
     public MenuBlock clickMainMenuItem() {
-        waitForVisibilityOfElement(firstMainMenuItems);
         LOGGER.info("Open menu {}", firstMainMenuItems.getText());
         jsClick(firstMainMenuItems);
         return this;
